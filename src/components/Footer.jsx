0cc87812e5405b23ec388d51443e0b1184f73c3d
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2025-06-10 14:29:12 +0530

    creating footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="text-center bg-black/80 p-4 text-white text-2xl max-md:text-lg font-bold">
+        Contact Details
+      </div>
+      <div className="flex "></div>
+    </>
+  );
+}
+
+export default Footer;
