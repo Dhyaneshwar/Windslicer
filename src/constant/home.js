4ae1df89ae28f9398fd40d8fee057d1723dc8944
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2025-06-10 14:28:52 +0530

    creating constants

---
+++ src/constant/home.js
@@ -0,0 +1,4 @@
+export const welcomeMsg = "Welcome to Windslicer paddles!";
+
+export const windslicerIntro =
+  "Windslicer paddles are hand made, Greenland-style sea kayak blades in manufacture since 1985. Standard, ‘storm’ or jointed paddles are available to order.";
---
+++ src/constant/nav-bar.js
@@ -0,0 +1,8 @@
+export const homeTooltip =
+  "Windslicer paddles and why narrow blades are good for sea kayaking.";
+
+export const galleryTooltip =
+  "Details of native paddles from museums and Windslicer paddles for comparison";
+
+export const safetyTooltip =
+  "Kayak test reports and articles concentrating on buoyancy design and  improvement in recovery potential. These reports are not available elsewhere.";
