6e8e7e780fe9d6060ca7d00ac42f60bc68425351
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2025-06-10 07:03:50 +0530

    integrating nav bar into application

--- src/components/NavLayout.jsx
+++ src/components/NavLayout.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { FloatingNav } from "../aceternityUi/floating-navbar";
+// import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 function NavLayout() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Gallery",
+      link: "/gallery",
+    },
+    {
+      name: "Safety",
+      link: "/safety",
+    },
+  ];
+
   return (
-    <>
-      <div>NavLayout</div>
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} />
       <Outlet />
-    </>
+    </div>
   );
 }
 
